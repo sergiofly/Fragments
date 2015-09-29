fcd0088fef101363c0a5375fa431aea80a246c3b
Sergio Mello <sergio@mello.to.it>
2015-09-29 11:06:31 +0800

    Create fragment java file

---
+++ app/src/main/java/it/to/mello/testing/fragments/TopSectionFragment.java
@@ -0,0 +1,20 @@
+package it.to.mello.testing.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.view.View;
+
+// need to inherit from the Fragment class (this is a fragment)
+public class TopSectionFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.top_section_fragment, container, false);
+        return view;
+    }
+}
+
